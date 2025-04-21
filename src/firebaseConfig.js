20fd02926acbfeae0e6e55ee32a841cc2aabb366
Kevin <laifongyi123@gmail.com>
2025-04-21 10:12:43 +0800

    7th update security

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3VAlJjA0e2xGytNF5_u4n0PLKhwc_oXQ",
-
-  authDomain: "fullstackproject-bdd9b.firebaseapp.com",
-
-  projectId: "fullstackproject-bdd9b",
-
-  appId: "1:182068037581:web:4a012bf1a1d738b85e2684",
-
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
